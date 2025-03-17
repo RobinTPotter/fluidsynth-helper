85169db7263886b22c839c9637d4e7ecd576c84b
robin <robin>
2025-03-17 20:13:50 +0000

    vel parse

--- vel100.py
+++ vel100.py
@@ -1,13 +1,21 @@
-
+import argparse
 import mido
 
-inn = [i for i in mido.get_input_names() if "24:0" in i][0]
+parser = argparse.ArgumentParser()
+parser.add_argument("-vo", "--velocity", required=False, type=int, default=None)
+parser.add_argument("-do", "--device-out", required=False, default="24:0")
+parser.add_argument("-di", "--device-in", required=False, default="128:0")
+
+args = parser.parse_args() 
+
+inn = [i for i in mido.get_input_names() if args.device_in in i][0]
 inport = mido.open_input(inn)
 
-outt = [i for i in mido.get_output_names() if "128:0" in i][0]
+outt = [i for i in mido.get_output_names() if args.device_out in i][0]
 outport = mido.open_output(outt)
 
 for msg in inport:
     if msg.type == 'note_on' and msg.velocity > 0:
-        msg.velocity = 10
+        if args.velocity: msg.velocity = args.velocity
+
     outport.send(msg)
